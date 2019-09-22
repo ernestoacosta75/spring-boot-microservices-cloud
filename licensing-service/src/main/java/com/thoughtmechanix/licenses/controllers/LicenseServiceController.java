731d01ef5931f9492d28a4a1ff9fbfe6d1537e20
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-09-22 22:02:17 +0200

    License Microservice: added Swagger 2 configuration

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
@@ -3,6 +3,10 @@ package com.thoughtmechanix.licenses.controllers;
 import com.thoughtmechanix.licenses.config.ServiceConfig;
 import com.thoughtmechanix.licenses.model.License;
 import com.thoughtmechanix.licenses.services.LicenseService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -26,14 +30,30 @@ public class LicenseServiceController {
     @Autowired
     private ServiceConfig serviceConfig;
 
+    @ApiOperation(value = "Get a list of licenses", response= List.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @GetMapping(value="/")
     public List<License> getLicenses( @PathVariable("organizationId") String organizationId) {
 
         return licenseService.getLicensesByOrg(organizationId);
     }
 
+    @ApiOperation(value = "Get an specific license based in its ID", response= License.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved license"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @GetMapping(value="/{licenseId}")
-    public License getLicenses( @PathVariable("organizationId") String organizationId,
+    public License getLicenses( @ApiParam(value = "The organization id", required = true)
+                                @PathVariable("organizationId") String organizationId,
+                                @ApiParam(value = "The license id", required = true)
                                 @PathVariable("licenseId") String licenseId) {
 
         return licenseService.getLicense(organizationId,licenseId);
@@ -56,26 +76,61 @@ public class LicenseServiceController {
      *                              a service via Ribbon
      * @return
      */
+    @ApiOperation(value = "Get an specific license using an specific discovery client", response= License.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved license"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @GetMapping(value="/{licenseId}/{clientType}")
-    public String getLicensesWithClient(@PathVariable("organizationId") String organizationId,
-                                         @PathVariable("licenseId") String licenseId,
-                                         @PathVariable("clientType") String clientType) {
+    public String getLicensesWithClient(@ApiParam(value = "The organization id", required = true)
+                                        @PathVariable("organizationId") String organizationId,
+                                        @ApiParam(value = "The license id", required = true)
+                                        @PathVariable("licenseId") String licenseId,
+                                        @ApiParam(value = "The client type", required = true)
+                                        @PathVariable("clientType") String clientType) {
         return String.format("This is the Delete");
     }
 
+    @ApiOperation(value = "Update a license", response= String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully updated license"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @PutMapping(value="/{licenseId}")
-    public String updateLicenses( @PathVariable("licenseId") String licenseId) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public String updateLicense( @ApiParam(value = "The license id", required = true)
+                                  @PathVariable("licenseId") String licenseId) {
         return String.format("This is the put");
     }
 
+    @ApiOperation(value = "Create a license")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully saved license"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @PostMapping(value="/")
-    public void saveLicenses(@RequestBody License license) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveLicense(@RequestBody License license) {
        licenseService.saveLicense(license);
     }
 
+    @ApiOperation(value = "Delete a license", response= String.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully deleted license"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @DeleteMapping(value="/{licenseId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public String deleteLicenses( @PathVariable("licenseId") String licenseId) {
+    public String deleteLicense( @ApiParam(value = "The license id", required = true)
+                                 @PathVariable("licenseId") String licenseId) {
         return String.format("This is the Delete");
     }
 }
