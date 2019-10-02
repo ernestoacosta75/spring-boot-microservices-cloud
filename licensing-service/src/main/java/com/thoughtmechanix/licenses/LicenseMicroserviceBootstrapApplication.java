5c134cae0202edb7589b987e5fe1e61de9b40e64
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2019-10-02 21:15:25 +0200

    Setting up the Licensing server to use Spring Cloud and Hystrix. Circuit breaker using Hystrix implemented.

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/LicenseMicroserviceBootstrapApplication.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/LicenseMicroserviceBootstrapApplication.java
@@ -3,9 +3,10 @@ package com.thoughtmechanix.licenses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -31,7 +32,9 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 //@EnableDiscoveryClient
 @RefreshScope
-@EnableFeignClients
+//@EnableFeignClients
+@EnableEurekaClient
+@EnableCircuitBreaker
 public class LicenseMicroserviceBootstrapApplication {
 
     @LoadBalanced
--- licensing-service/src/main/java/com/thoughtmechanix/licenses/services/impl/LicenseServiceImpl.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/services/impl/LicenseServiceImpl.java
@@ -1,5 +1,6 @@
 package com.thoughtmechanix.licenses.services.impl;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.thoughtmechanix.licenses.clients.OrganizationDiscoveryClient;
 import com.thoughtmechanix.licenses.clients.OrganizationFeignClient;
 import com.thoughtmechanix.licenses.clients.OrganizationRestTemplateClient;
@@ -13,8 +14,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
+/**
+ * Hystrix and Sprig Cloud use  the<b>@HystrixCommand</b> annotation
+ * to mark Java class methods as being managed by a Hystrix circuit breaker.
+ *
+ * When the Spring framework sees this annotation, it will dinamically
+ * generate a proxy that will wrapper the method and manage all calls to that
+ * method through a thread pool of threads specifically set aside to handle
+ * remote calls.
+ *
+ * @author Ernesto A. Rodriguez Acosta
+ */
 @Service
 @Slf4j
 public class LicenseServiceImpl implements LicenseService {
@@ -42,8 +55,11 @@ public class LicenseServiceImpl implements LicenseService {
         return license;
     }
 
+    @HystrixCommand
     @Override
     public List<License> getLicensesByOrg(String organizationId){
+        randomlyRunLong();
+
         return licenseRepository.findByOrganizationId( organizationId );
     }
 
@@ -101,5 +117,33 @@ public class LicenseServiceImpl implements LicenseService {
         }
 
         return organization;
+    }
+
+    /**
+     * This method gives a one in three
+     * chance of a database call running long.
+     */
+    private void randomlyRunLong() {
+        Random random = new Random();
+
+        int randomNum = random.nextInt((3 - 1) + 1) * 1;
+
+        if(randomNum == 3) {
+           sleep();
+        }
+    }
+
+    /**
+     * To sleep for 11000 milliseconds (11 seconds).
+     * Default Hystrix behavior is to time a call after
+     * 1 second.
+     */
+    private void sleep() {
+        try {
+            Thread.sleep(11000);
+        }
+        catch(InterruptedException ie) {
+            ie.printStackTrace();
+        }
     }
 }
